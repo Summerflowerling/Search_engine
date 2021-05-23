7f28aa86d331dec2321e8337a0161719c2364e17
yuling <yulinglin@protonmail.com>
2021-05-23 16:27:41 +0800

    refactor:Add new div to display fetching data.

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -2,9 +2,24 @@ const resultIntro = document.getElementById("result-intro")
     
 
 export function updateUi (sentBackData){
-
-    resultIntro.innerHTML = `${sentBackData.query}`
+    if(resultIntro.hasChildNodes()){
+        resultIntro.removeChild(newDiv);
+    } 
     
-   
+    console.log("UpdateUi")
+    console.log("sentBackData length",sentBackData.length)
+    let count = 0
+    while(count < sentBackData.length){
+        let newDiv = document.createElement("div")
+        newDiv.setAttribute("class", "result-list")
+        newDiv.innerHTML = `
+        <h3>${sentBackData[count].title}</h3>
+        <p>${sentBackData[count].snippet}</p>
+        `
+        resultIntro.appendChild(newDiv)
+        count++
+
+    }
+  
 
 }
