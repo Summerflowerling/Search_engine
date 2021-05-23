bcb468d0458d120ef2fe06ec1107e8779e65b81f
yuling <yulinglin@protonmail.com>
2021-05-23 16:25:06 +0800

    refactor: Server port does not match. Show 404 not found.
    Specify the GET url to match with express server port. Peoject works fine.

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -4,14 +4,15 @@ export async function handleSubmit (search){
     console.log(search)
 
     //const url = `/getInput`
-    const url = `/getInput/${search}`
+    const url = `http://localhost:8086/getInput/${search}`
     //console.log(url)
     //const data = await fetch (url)
     const response = await fetch(url)
     const json = await response.json()
-    console.log(json)
+    console.log("This is client side return data",json.query.search[0])
+    const displayData = json.query.search
     try{
-        Client.updateUi(json)
+        Client.updateUi(displayData)
     }
     catch(err){
         console.log("There is an error.", err)
