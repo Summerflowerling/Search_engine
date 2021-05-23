71663149208793c61bb03269397d5db95b17287f
yuling <yulinglin@protonmail.com>
2021-05-23 16:38:25 +0800

    refactor: Remove exiting content when making another search.
    Remove exsiting seach list items by empty resultIntro section's innerHTML in updateUi.js

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -3,23 +3,22 @@ const resultIntro = document.getElementById("result-intro")
 
 export function updateUi (sentBackData){
     if(resultIntro.hasChildNodes()){
-        resultIntro.removeChild(newDiv);
+       resultIntro.innerHTML =``
     } 
     
-    console.log("UpdateUi")
-    console.log("sentBackData length",sentBackData.length)
-    let count = 0
-    while(count < sentBackData.length){
-        let newDiv = document.createElement("div")
-        newDiv.setAttribute("class", "result-list")
-        newDiv.innerHTML = `
-        <h3>${sentBackData[count].title}</h3>
-        <p>${sentBackData[count].snippet}</p>
-        `
-        resultIntro.appendChild(newDiv)
-        count++
-
+        let count = 0
+        while(count < sentBackData.length){
+            let newDiv = document.createElement("div")
+            newDiv.setAttribute("class", "result-list")
+            newDiv.innerHTML = `
+            <h3>${sentBackData[count].title}</h3>
+            <p>${sentBackData[count].snippet}</p>
+            `
+            resultIntro.appendChild(newDiv)
+            count++
+    
+        
+      
     }
-  
 
 }
