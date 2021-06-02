53a250eb4b81c7bebfd5808930a021be788e5829
yuling <yulinglin@protonmail.com>
2021-06-02 19:28:13 +0800

    style: Delete unnecessary comments.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,4 @@
 
-
-/*Api URL*/
 let projectData ={}
 var path = require('path')
 
@@ -24,10 +22,7 @@ app.use(bodyParser.json());
 const cors = require("cors")
 app.use(cors())
 
-/*Experiment*/
-/*Try adding router*/
-//const router = express.Router()
-//app.use("/", router)
+
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -39,7 +34,7 @@ app.get('/', function (req, res) {
  app.get('/getInput/:searchTerm', async function(req,res){
     const userInput = req.params.searchTerm
     const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&generator=search&inprop=url&gsrsearch=${userInput}&gsrprop=snippet`
-    //const url =`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${userInput}`
+    
     const data = await fetch (url)
 
     try{
@@ -60,53 +55,4 @@ app.get('/', function (req, res) {
 
 
 module.exports = app
-
-/*NOTE: replace the api key with stored variable will crush, need help to debug */
-
-/*
-app.post("/getGeoname", async function(req,res){
-   
-    const API_URL = `http://api.geonames.org/searchJSON?q=${req.body.location}&maxRows=1&username=iku124` 
-
-    const myPromise = await fetch(API_URL); 
-    
-    try{
-        const myData = await myPromise.json();  
-        let lng = myData.geonames[0].lng
-        let lat = myData.geonames[0].lat
-        
-        const weatherbit = await fetch (weatherBitUrl+`lat=${lat}&lon=${lng}&days=7&key=25236c54b21347c5acba0d34020a5c84`)
-       
-        try{
-            const weatherbitPromise = await weatherbit.json()
-            console.log(weatherbitPromise)
-            const pixabay = await fetch(`https://pixabay.com/api/?key=4772361-58a041a9c4a31b16cbe90fbc1&q=${req.body.location}&image_type=photo&editors_choice=true&category=travel`)
-
-            try{
-                const pixabayPrimose = await pixabay.json()
-                console.log(pixabayPrimose.hits[0])
-                res.send([weatherbitPromise,pixabayPrimose.hits[0].largeImageURL])
-
-            } catch(error){
-                res.send([weatherbitPromise, "img/backup.png"]) 
-                console.log("Something wrong when fetching the photo",error)
-            }
-
-            
-        } 
-        catch(err){
-            console.log("Error in the second fetch",err)
-        }
-       
-    }
-       
-    catch(error){
-        alert("Make sure you enter a country or city name")
-        return;
-    }
-
-})
-
-*/
-
 
