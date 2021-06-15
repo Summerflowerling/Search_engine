9ad12b57f6b59d8d5e5a1fdc60258ba2f22e592e
yuling <yulinglin@protonmail.com>
2021-06-15 19:40:33 +0800

    style: reformat

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,15 +1,15 @@
 //import { search } from "../../server/server"
 
-
-
 export async function handleSubmit (search){
     console.log(search)
     //const url = `/getInput` doesn't work with dev server
     //NOTE: in order to work with dev derver, the fetching url needs to be port specific like below
-    const url = `http://localhost:8086/getInput/${search}`
+    //const url = `http://localhost:8086/getInput/${search}`
+    const url = `./getInput/${search}`
     const response = await fetch(url)
     const json = await response.json()
     console.log("This is client side return data",json.query.pages)
+    console.log("This is client side return json data",json)
     const displayData = Object.values(json.query.pages) //convert return obj into an array
     
     try{
@@ -18,10 +18,9 @@ export async function handleSubmit (search){
     }
     catch(err){
         console.log("There is an error.", err)
+        console.log("This is client side return json data",json)
     }
     
-     
-
 }
 
 
