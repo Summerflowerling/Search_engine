4beb912345666e125e893e5e0f7b186dc57b99e1
yuling <yulinglin@protonmail.com>
2021-06-15 19:42:41 +0800

    style: reformat

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -1,27 +1,24 @@
 const resultIntro = document.getElementById("result-intro")
     
-
 export function updateUi (sentBackData){
     if(resultIntro.hasChildNodes()){
        resultIntro.innerHTML =``
     } 
     
-        let count = 0
-        while(count < sentBackData.length){
-            let url = sentBackData[count].fullurl
-            let newDiv = document.createElement("div")
-            newDiv.setAttribute("class", "result-list")
-            newDiv.innerHTML = `
-            <a href=${url} target="_blank">
-            <h3 class="list-item-title">${sentBackData[count].title}</h3>
-            <p>${sentBackData[count].snippet}...</p>
-            </a>
-            `
-            resultIntro.appendChild(newDiv)
-            count++
+    let count = 0
+    while(count < sentBackData.length){
+        let url = sentBackData[count].fullurl
+        let newDiv = document.createElement("div")
+        newDiv.setAttribute("class", "result-list")
+        newDiv.innerHTML = `
+        <a href=${url} target="_blank">
+        <h3 class="list-item-title">${sentBackData[count].title}</h3>
+        <p>${sentBackData[count].snippet}...</p>
+        </a>
+        `
+        resultIntro.appendChild(newDiv)
+        count++
     
-        
-      
     }
 
 }
