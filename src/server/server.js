fa669d678da99ddfd21167af56119037cfd43bf4
yuling <yulinglin@protonmail.com>
2021-06-22 15:21:31 +0800

    Style: reformat in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,19 @@
 
-let projectData ={}
+let projectData = {}
 var path = require('path')
-
-/*.env and api keys*/
 const dotenv = require('dotenv');
-dotenv.config();
-
-
 const express = require('express')
-const app = express() 
-const fetch = require ('node-fetch')
-app.use(express.static('dist'))
-
-
-
+const app = express()
+const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
 const cors = require("cors")
-app.use(cors())
 
-
+dotenv.config();
+app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -31,28 +22,22 @@ app.get('/', function (req, res) {
 
 
 
- app.get('/getInput/:searchTerm', async function(req,res){
+// set up GET router
+app.get('/getInput/:searchTerm', async function(req, res) {
     const userInput = req.params.searchTerm
     const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&generator=search&inprop=url&gsrsearch=${userInput}&gsrprop=snippet`
-    
-    const data = await fetch (url)
 
-    try{
+    const data = await fetch(url)
+
+    try {
         const json = await data.json()
         projectData = json
         res.send(projectData)
         console.log("Json Data got", json)
-        
+    } catch (error) {
+        console.log("You got error:", error)
     }
-    catch(error){
-        console.log("You got error:",error)
-    }
-    
- 
-
 
 })
 
-
 module.exports = app
-
