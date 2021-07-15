b3222861d0d26eb930e5f45ccd059d2ff97c133e
Yuling <yulinglin@protonmail.com>
2021-07-15 08:52:50 +0800

    style: delete empty spaces

--- src/_test_/route.spec.js
+++ src/_test_/route.spec.js
@@ -1,9 +1,6 @@
 const request = require('supertest')
 const app = require('../server/server.js')
 
-
-
-
 describe('GET / serachTerm', function() { 
   it('respond with json', function(done) { 
     request(app) 
